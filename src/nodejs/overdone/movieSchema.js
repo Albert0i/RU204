0611f9a6b204def1ad1ade0d70b1c3790fec6f93
Alberto Iong <albert0i@hotmail.com>
2024-04-24 15:34:59 +0800

    add overdone folder

---
+++ src/nodejs/overdone/movieSchema.js
@@ -0,0 +1,33 @@
+import 'dotenv/config'
+import { Schema } from 'redis-om'
+
+/*
+    Imagine we're operating a video rental store. We've decided to represent each of the movies in our inventory using JSON documents that have the following structure:
+
+    {
+    "title": "The Matrix",
+    "released": 1999,
+    "runtime": 137,
+    "stars": [
+        "Keanu Reeves",
+        "Laurence Fishburne",
+        "Carrie-Ann Moss"
+    ],
+    "Summary": "A stranger leads computer hacker Neo to a forbidding underworld, he discovers the truth: the life he knows is the elaborate deception of an evil cyber-intelligence.",
+    rating: 8.7,
+    inStock: true
+    } 
+*/
+
+export const movieSchema = new Schema(process.env.REDIS_OM_PREFIX + "movie", {
+        title: { type: 'string' },
+        released: { type: 'number' }, 
+        runtime: { type: 'number'}, 
+        stars:  { type: 'string[]' },
+        summary: { type: 'string' }, 
+        rating: { type: 'number' },
+        inStock: { type: 'boolean' }
+    }, 
+    {
+        dataStructure: process.env.REDIS_OM_DATASTRUCTURE
+    });
---
+++ src/nodejs/overdone/redisClient.js
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import { createClient } from 'redis'
+
+const redisClient = new createClient({
+        url: process.env.REDIS_OM_URL || 'redis://localhost:6379'
+    });
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+
+await redisClient.connect()
+const pong = await redisClient.ping()
+console.log(pong)
+
+export { redisClient }
