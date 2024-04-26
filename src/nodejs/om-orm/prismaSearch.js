62b362d91586fef79823517e0be9d74ef3979582
Alberto Iong <albert0i@hotmail.com>
2024-04-26 15:44:00 +0800

    finish with prismaSearch.js

--- src/nodejs/om-orm/prismaSearch.js
+++ src/nodejs/om-orm/prismaSearch.js
@@ -19,15 +19,37 @@ console.log(movies)
 //    select 'title' and 'stars'
 movies = await prisma.movie.findMany({
      where: { OR: [
-        {stars: { contains: "Morgan Freeman" }} , 
-        {stars: { contains: "Leonardo DiCaprio" }} 
+        { stars: { contains: "Morgan Freeman" } } , 
+        { stars: { contains: "Leonardo DiCaprio" } } 
      ] },
      select: { title: true, stars: true },
    })
 console.log(movies)
 
-// 3. 
+// 3. Find all movies with rating 7~8 (inclusive)
+//    select 'title' and 'rating'
+movies = await prisma.movie.findMany({
+   where: { AND: [
+      { rating: { gte: 7 } }, 
+      { rating: { lte: 8 } }
+   ] },
+   select: { title: true, rating: true },
+ })
+console.log(movies)
 
+// 4. Find all movie with a summary that contains 'crime' or 'police'.
+//    select 'id', 'title' and 'summary' 
+movies = await prisma.movie.findMany({
+   where: {
+      summary: {
+         search: 'police crime',
+      },
+    },
+    select: { id: true, title: true, summary: true }
+ })
+console.log(movies)
+
+// These are pretty much all I can do with single file search in prisma... 
 
 /*
    Filtering and Sorting
@@ -35,4 +57,10 @@ console.log(movies)
 
    Prisma Client API reference
    https://www.prisma.io/docs/orm/reference/prisma-client-reference#filter-conditions-and-operators
+
+   Full-text search
+   https://www.prisma.io/docs/orm/prisma-client/queries/full-text-search
+
+   14.9 Full-Text Search Functions
+   https://dev.mysql.com/doc/refman/8.0/en/fulltext-search.html
 */
