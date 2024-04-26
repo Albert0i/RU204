7751f20c200fa8870bf1055e61d7c544697c8921
Alberto Iong <albert0i@hotmail.com>
2024-04-26 17:40:52 +0800

    minir tipo fix

--- src/nodejs/om-orm/om/seed.js
+++ src/nodejs/om-orm/om/seed.js
@@ -10,7 +10,11 @@ async function main() {
    let response
    // Erase old data... 
    console.log(`Removing index ${movieSchema.indexName}...`)
-   // Dropping an index and all accompanied documents 
+
+   // Attention: 
+   // As for the first time of seeding, you may need to comment the 'FT.DROP' below;
+   // 'FT.DROP' seems an undocumented feature not mention in any redis documentation.
+   // Dropping an index and all accompanied documents, which seems to be an 
    response = await redisClient.sendCommand(['FT.DROP', `${movieSchema.indexName}`])
    // Redis-OM uses a string to keep track of index re-creation. 
    response = await redisClient.sendCommand(['DEL', `${movieSchema.indexName}:hash`])
