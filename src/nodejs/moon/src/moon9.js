7166e968e46a1f77ec28d0143e90cd47a2abaa59
Alberto Iong <albert0i@hotmail.com>
2024-05-07 17:35:31 +0800

    have to use redis stack

--- src/nodejs/moon/src/moon9.js
+++ src/nodejs/moon/src/moon9.js
@@ -5,35 +5,46 @@ const jsonObject = {
   name: 'Roberta McDonald',
   pets: [
     {
-    name: 'Rex',
-    species: 'dog',
-    age: 3,
-    isMammal: true
+      name: 'Fluffy',
+      species: 'dog',
+      age: 5,
+      isMammal: true
     },
     {
-    name: 'Goldie',
-    species: 'fish',
-    age: 2,
-    isMammal: false
+      name: 'Rex',
+      species: 'dog',
+      age: 3,
+      isMammal: true
+    },
+    {
+      name: 'Goldie',
+      species: 'fish',
+      age: 2,
+      isMammal: false
     }
-  ]
+  ],
+  address: {
+    number: 99,
+    street: 'Main Street',
+    city: 'Springfield',
+    state: 'OH',
+    country: 'USA'
+  }
 }
 
 await redisClient.json.set('noderedis:jsondata', '$', jsonObject)
-//await redisClient.json.set('mykey', '$', '{"name":"Example"}');
-//await redisClient.sendCommand([ "JSON.SET", "myket", "$", '{"name":"Example"}' ]) 
-
 // Retrieve the name and age of the second pet in the pets array.
-// let results = await client.json.get('noderedis:jsondata', {
-//     path: [
-//       '$.pets[1].name',
-//       '$.pets[1].age'
-//     ]
-//   });
+let results = await redisClient.json.get('noderedis:jsondata', {
+    path: [
+      '$.pets[1].name',
+      '$.pets[1].age'
+    ]
+  });
   
   // { '$.pets[1].name': [ 'Rex' ], '$.pets[1].age': [ 3 ] }
-  //console.log(results);
+console.log(results);
 
+await redisClient.del('noderedis:jsondata')
 await redisClient.disconnect()
 
 /*
