96fa7e1ac6e99530e47608ac367de436c357dfa8
Alberto Iong <albert0i@hotmail.com>
2024-05-14 11:02:22 +0800

    attempt to use jest

---
+++ src/nodejs/moon/tests/hello.test.js
@@ -0,0 +1,29 @@
+
+import { redisClient } from "../src/config/redisClient.js";
+
+const testSuiteName = 'Hello';
+const testKey = 'hello';
+
+/* eslint-disable no-undef */
+
+beforeAll(() => {
+  //jest.setTimeout(60000);
+});
+
+afterAll(async () => {
+  // Delete the key we may have created.
+  await redisClient.del(testKey);
+
+  // Release Redis connection.
+  redisClient.disconnect();
+});
+
+test(`${testSuiteName}: basic hello world test`, async () => {
+  const reply = await redisClient.set(testKey, 'world');
+  expect(reply).toBe('OK');
+
+  const keyValue = await redisClient.get(testKey);
+  expect(keyValue).toBe('world');
+});
+
+/* eslint-enable */
